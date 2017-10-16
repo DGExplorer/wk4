52cd9fb0c2147b84927c08abb47911e4442b8d90
Dora <dg457@njit.edu>
2017-10-16 17:16:50 -0400

    added one

--- index.php
+++ index.php
@@ -9,4 +9,19 @@ $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year)
 
+echo "<br>"#2"<br>";
+$date = str_replace('-', '/', $date);
+echo $date; .  "is the new formated date.<br>";
+
+echo "<br>"#3"<br>";
+$result = strnatcasecmp($date, $tar);
+if ($result > 0) {
+	echo "the future"<br>";
+}	elseif ($result < 0) {
+	echo "the past"<br>";
+}	elseif ($result = 0) {
+	echo "Ooops."<br>";
+}
+
+
 ?>
