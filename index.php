dc21c7d510368d60f5210588fec11e2e16dc8621
Dora <dg457@njit.edu>
2017-10-18 16:41:39 -0400

    added things

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors','On');
+error_reporting(E_ALL);
 
 $date =  date('Y-m-d', time());
 echo "The value of \$date: ".$date."<br>";
@@ -10,28 +12,20 @@ $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year);
 
-// Begin programming excercises 
-echo "<br>";
 
-// dont want to overwrite original vars
-$dateOriginal = $date; 
-
-// #2
 echo "<br>#2<br>";
 $date = str_replace('-', '/', $date);
 echo $date . " is the new formatted date.<br>";
 
-// #3
 echo "<br>#3<br>";
 if ($date > $tar) echo "The Future <br>";
 elseif ($date < $tar) echo "The Past <br>";
 else echo "Oops <br>";
 
-// #4
 echo "<br>#4<br>";
 $lastPos = 0;
 $positions = array();
-while (($lastPos = strpos($dateOriginal, "-", $lastPos))!== false) {
+while (($lastPos = strpos($date, "-", $lastPos))!== false) {
     $positions[] = $lastPos;
     $lastPos = $lastPos + strlen("-");
 }
@@ -40,13 +34,31 @@ foreach ($positions as $value) {
     $dateOriginal[$value] = " ";
 }
 
-// #5
 echo "<br>#5<br>";
 $dateLength = array();
 $dateLength = (explode(" ",$dateOriginal));
 echo count($dateLength) . " words in \$date string<br/>";
 
+echo "<br>#6<br>";
+$length = strlen($date);
+echo "Length of \$date string: " . $length . "<br>";
 
-}
+echo "<br>#7<br>";
+echo "ASCII of 1st character in \$date: " . ord($date[0]) . "<br>";
+
+echo "<br>#8<br>";
+$part = substr($date, 8, 2);
+echo "The last two characters in \$date are: " . $part . "<br>";
+
+echo "<br>#9<br>";
+$dateNewArr = array();
+$dateNewArr = explode('/', $date);
+foreach ($dateNewArr as $i) 
+    echo $i . " ";
+
+echo "<br>";
+
+
+
 
 ?>
