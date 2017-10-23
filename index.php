5dcf15c5e88ca50137d52e444fb85bc0dcaca868
Dora <dg457@njit.edu>
2017-10-23 16:25:51 -0400

    add codes for all

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('display_errors','On');
-error_reporting(E_ALL);
 
 $date =  date('Y-m-d', time());
 echo "The value of \$date: ".$date."<br>";
@@ -11,7 +9,9 @@ echo "The value of \$tar: ".$tar."<br>";
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year);
+echo"<br>";
 
+$dateOriginal = $date;
 
 echo "<br>#2<br>";
 $date = str_replace('-', '/', $date);
@@ -27,24 +27,54 @@ if ($result > "0") {
     echo "Ooops.<br>";
 }
 
+echo "<br>#4<br>";
+$position = strpos($date, "/"); 
+echo $position . " ";
+$position2 = strpos($date, "/", $position+1); 
+echo $position2 . " ";
+$position3 = strpos($date, "/", $position2+1); 
+echo $position3 . "<br>";
 
 echo "<br>#5<br>";
 $dateLength = array();
-$dateLength = (explode(" ",$dateOriginal));
+$dateLength = (explode("-",$dateOriginal));
 echo count($dateLength) . " words in \$date string<br/>";
 
 echo "<br>#6<br>";
 $length = strlen($date);
-echo "Length of \$date string: " . $length . "<br>";
+echo "Length of \$date string is: " . $length . "<br>";
 
 echo "<br>#7<br>";
-echo "ASCII of 1st character in \$date: " . ord($date[0]) . "<br>";
+echo "ASCII of the first character in \$date is: " . ord($date[0]) . "<br>";
 
 echo "<br>#8<br>";
 $part = substr($date, 8, 2);
 echo "The last two characters in \$date are: " . $part . "<br>";
 
+echo "<br>#9<br>";
+$dateNewArr = array();
+$dateNewArr = explode('/', $date);
+foreach ($dateNewArr as $i) 
+    echo $i ." ";
 
+echo "<br><br>#10<br>";
 
+$testDate = date_create('2017-10-01');
+foreach ($year as $j) {
+  $testDate->setDate($j, 1, 1); //set the year to the year being tested
 
+  switch($testDate->format('L')) { //use the php builtin test for leap year
+	  case '0':
+	    echo "False ";
+	    break;
+	  case '1':
+	    echo "True ";
+	    break;
+	  default:
+	    echo "? ";
+	  
+  }
+}
+
+	
 ?>
